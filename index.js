ce2c7c97a9cadd06c47fcac5d4289b094b17a989
brightonchege <brightonspak@gmail.com>
2022-08-13 21:57:13 +0300

    loops complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+
+function writeCards(name,event){ 
+  const thank = []
+  for(let i = 0; i < name.length; i++){
+    thank[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+  }
+  return thank
+
+}
+
+
+
+
+
+function countDown(){
+    let count = 10;
+    while (count >= 0) {
+      console.log(count--);
+}
+}
+countDown()
+
+
+
